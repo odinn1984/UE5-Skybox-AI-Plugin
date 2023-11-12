673000af43253f6a8baa89872721008f17c9d20b
odinn1984 <levan@sleep-coding.com>
2023-11-12 03:50:16 +0200

    feat: add input dialog widget for skybox id

---
+++ BlockadeLabsSkyboxAi/Source/BlockadeLabsSkyboxAi/Private/InputDialogWidget.cpp
@@ -0,0 +1,91 @@
+﻿#include "InputDialogWidget.h"
+
+DEFINE_LOG_CATEGORY(InputDialogWidget);
+
+void SInputDialogWidget::Construct(const FArguments &InArgs)
+{
+  ParentWindow = InArgs._ParentWindow;
+  DialogTitle = InArgs._DialogTitle;
+  OnConfirm = InArgs._OnConfirm;
+  OnCancel = InArgs._OnCancel;
+
+  ChildSlot
+  [
+    SNew(SVerticalBox)
+
+    + SVerticalBox::Slot()
+      .Padding(10)
+      .AutoHeight()
+    [
+      SAssignNew(InputField, SEditableTextBox)
+    ]
+
+    + SVerticalBox::Slot()
+      .Padding(10)
+      .AutoHeight()
+    [
+      SNew(SHorizontalBox)
+
+      + SHorizontalBox::Slot()
+      .AutoWidth()
+      [
+        SNew(SButton)
+            .Text(FText::FromString(TEXT("Confirm")))
+            .OnClicked_Lambda(
+                       [this]() -> FReply
+                       {
+                         if (!IsInputValid())
+                         {
+                           FMessageDialog::Open(
+                             EAppMsgCategory::Error,
+                             EAppMsgType::Ok,
+                             FText::FromString(TEXT("ID must be >= 0")),
+                             SInputDialogWidget::ValidationErrorTitle
+                             );
+                         }
+                         else
+                         {
+                           uint32 Id;
+                           LexTryParseString(Id, *InputField->GetText().ToString());
+                           // ReSharper disable once CppExpressionWithoutSideEffects
+                           OnConfirm.ExecuteIfBound(Id);
+                           ParentWindow->RequestDestroyWindow();
+                         }
+
+                         return FReply::Handled();
+                       }
+                       )
+      ]
+
+      + SHorizontalBox::Slot()
+      .AutoWidth()
+      [
+        SNew(SButton)
+            .Text(FText::FromString(TEXT("Cancel")))
+            .OnClicked_Lambda(
+                       [this]() -> FReply
+                       {
+                         // ReSharper disable once CppExpressionWithoutSideEffects
+                         OnCancel.ExecuteIfBound();
+                         ParentWindow->RequestDestroyWindow();
+                         return FReply::Handled();
+                       }
+                       )
+      ]
+    ]
+  ];
+}
+
+bool SInputDialogWidget::IsInputValid() const
+{
+  int32 Id;
+  const bool bInitialValidation = InputField.IsValid() && !InputField->GetText().IsEmptyOrWhitespace();
+  const bool bParseSuccess = LexTryParseString(Id, *InputField->GetText().ToString());
+
+  if (bParseSuccess)
+  {
+    return Id >= 0 && bInitialValidation;
+  }
+
+  return bInitialValidation && bParseSuccess;
+}
---
+++ BlockadeLabsSkyboxAi/Source/BlockadeLabsSkyboxAi/Public/InputDialogWidget.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(InputDialogWidget, Log, All);
+
+DECLARE_DELEGATE(FOnInputDialogCancel);
+DECLARE_DELEGATE_OneParam(FOnInputDialogConfirm, const uint32);
+
+class SInputDialogWidget : public SCompoundWidget
+{
+public:
+  SLATE_BEGIN_ARGS(SInputDialogWidget)
+    {
+    }
+
+    SLATE_ARGUMENT(TSharedPtr<SWindow>, ParentWindow)
+    SLATE_ARGUMENT(FText, DialogTitle)
+    SLATE_EVENT(FOnInputDialogCancel, OnCancel)
+    SLATE_EVENT(FOnInputDialogConfirm, OnConfirm)
+  SLATE_END_ARGS()
+
+  inline static const FText ValidationErrorTitle = FText::FromString(TEXT("Error"));
+
+  void Construct(const FArguments &InArgs);
+
+private:
+  FText DialogTitle;
+  FOnInputDialogCancel OnCancel;
+  FOnInputDialogConfirm OnConfirm;
+  TSharedPtr<SWindow> ParentWindow;
+  TSharedPtr<SEditableTextBox> InputField;
+
+  bool IsInputValid() const;
+};
